7b0af53ff12503d49fda6d0ca7dbaf38c88c0c0b
dheaiai <44894579+dheaiai@users.noreply.github.com>
2023-07-25 16:38:44 +0200

    Update login_page.py

--- test/login_page.py
+++ test/login_page.py
@@ -8,18 +8,21 @@ class FormAuthentication(APIOps):
     SubmitBtn = ["ID", "send2"]
     mainmenu = ["CLASS_NAME", "ui.nav.items"]
 
-    def check_username_presence(self, username, timeout):
-        return self.presence_of_element_located(username, timeout)
+    def check_element_presence(self, element_name):
+        if element_name == "username":
+          return self.presence_of_element_located(username, 5)
+        elif element_name == "mainmenu":
+          return self.presence_of_element_located(mainmenu, 10)
       
-    def enter_username(self, username, TestData)  
+    def enter_username(self, TestData)  
         username_textbox = self.find_element(username)
         username_textbox.send_keys(TestData['username'])
 
-    def enter_password(self, password, TestData)
+    def enter_password(self, TestData)
         password_textbox = self.find_element(password)
         password_textbox.send_keys(TestData['password'])
 
-    def press_submit_button(self, SubmitBtn)
+    def press_submit_button(self)
         SignIn_button = self.find_element(SubmitBtn)
         SignIn_button.submit()
       
