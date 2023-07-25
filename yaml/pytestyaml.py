178250c321300c649ae1e3f058f55eb0c7cd509a
dheaiai <44894579+dheaiai@users.noreply.github.com>
2023-07-25 15:45:15 +0200

    Update pytestyaml.py

--- yaml/pytestyaml.py
+++ yaml/pytestyaml.py
@@ -1,12 +1,13 @@
 import yaml
 
+# Write data to yaml
 def writeyaml(dict,filename):
     filename = 'yaml/'+filename+ '.yaml'
     file = open("yaml/search_data.yaml", "w")
     yaml.dump(dict, file)
 
 # real data from yaml file
-def readyaml(filename, toFetchTestData):
+def readyaml(toFetchTestData, filename):
     filename = 'yaml/'+filename+ '.yaml'
     with open(filename, 'r') as file:
         TestData = yaml.safe_load(file)
