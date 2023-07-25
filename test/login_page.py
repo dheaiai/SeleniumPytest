9a6533a905a5a0a59234a15d84b8877f76272dd7
dheaiai <44894579+dheaiai@users.noreply.github.com>
2023-07-25 16:20:02 +0200

    Update login_page.py

--- test/login_page.py
+++ test/login_page.py
@@ -1,7 +1,7 @@
 # Library
-from APILayer.API import *
+from APILayer.API import APIOps
 
-class FormAuthentication(CommonOps):
+class FormAuthentication(APIOps):
   
     username = ["NAME", "login[username]"]
     password = ["NAME", "login[password]"]
@@ -9,16 +9,16 @@ class FormAuthentication(CommonOps):
     mainmenu = ["CLASS_NAME", "ui.nav.items"]
 
     def check_element_presence(self, username, timeout):
-        return presence_of_element_located(username, timeout)
+        return self.presence_of_element_located(username, timeout)
       
     def enter_username(self, username, TestData)  
-      username_textbox = find_element(username)
-      username_textbox.send_keys(TestData['username'])
+        username_textbox = self.find_element(username)
+        username_textbox.send_keys(TestData['username'])
 
     def enter_password(self, password, TestData)
-      password_textbox = find_element(password)
-      password_textbox.send_keys(TestData['password'])
+        password_textbox = self.find_element(password)
+        password_textbox.send_keys(TestData['password'])
 
-    def press_submit_button(SubmitBtn)
-      SignIn_button = find_element(SubmitBtn)
-      SignIn_button.submit()
+    def press_submit_button(self, SubmitBtn)
+        SignIn_button = self.find_element(SubmitBtn)
+        SignIn_button.submit()
