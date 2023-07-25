a7f76791592d1f37b58d509dfc4213cd8dbbd88d
dheaiai <44894579+dheaiai@users.noreply.github.com>
2023-07-25 16:58:49 +0200

    Update test_validation.py

--- test_validation.py
+++ test_validation.py
@@ -1,16 +1,40 @@
 from test.login_page import FormAuthentication
+from yaml.pytestyaml import *
+from tools.logs import LogInformation
 
 def test_form_authentication(driver):
+    functionname = inspect.currentframe().f_code.co_name
+    loggingData = LogInformation(namefile=functionname)
+    loggingData.TEST_INFORMATION(namefile=functionname, message="Class Name - " + functionname) 
+    yamlconfig="config_pytest"
   
     form = FormAuthentication(driver)
-
+    
+    TestData = readyaml(functionname, yamlconfig)    
+    loggingData.TEST_INFORMATION(namefile=functionname, message="url - " + TestData['url'])    
     form.open_url_to_navigate(TestData)
-    form.check_element_presence("username")
-    form.enter_username(TestData)
+    assert TestData['url'], driver.current_url
+  
+    bPresence = form.check_element_presence("username")
+    if bPresence is true:
+      loggingData.TEST_INFORMATION(namefile=functionname, message="username - " + TestData['username'])
+      form.enter_username(TestData)
+    else:
+      loggingData.TEST_INFORMATION(namefile=functionname, message="username took too much time to load")
+      
     form.enter_password(TestData)
+    loggingData.TEST_INFORMATION(namefile=functionname, message="password - " + TestData['password'])
+  
     form.press_submit_button()
 
-    assert "logged in" in form.check_login_logout_status().text 
-
-    form.click_logout_button()
-    assert "logged out" in form.check_login_logout_status().text
+    bPresence = form.check_element_presence("mainmenu")
+    if bPresence is true:
+      actualUrl = driver.current_url
+      expectedUrl = TestData['expected']
+      assert expectedUrl, actualUrl
+      loggingData.TEST_INFORMATION(namefile=functionname, message="expectedUrl - " + expectedUrl)
+      loggingData.TEST_INFORMATION(namefile=functionname, message="actualUrl - " + actualUrl)  
+    else:
+      loggingData.TEST_INFORMATION(namefile=functionname, message="main menu took too much time to load")
+      
+    loggingData.TEST_INFORMATION(namefile=functionname, message="*******End test - test_check_login********")
