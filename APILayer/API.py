44643dfa2647362c7400d4c569f2ad22d5bb2c14
dheaiai <44894579+dheaiai@users.noreply.github.com>
2023-07-25 16:17:32 +0200

    Update API.py

--- APILayer/API.py
+++ APILayer/API.py
@@ -5,35 +5,41 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
 
-def by_locator(byclass):
-    if byclass == "NAME":
-        return By.NAME
-    elif byclass == "ID":
-        return By.ID
-    elif byclass == "CLASS_NAME":
-        return By.CLASS_NAME
-    elif byclass == "XPATH":
-        return By.XPATH
-    elif byclass == "TAG_NAME":
-        return By.TAG_NAME
+class APIOps:
 
-
-def presence_of_element_located(driver, element, timeout):
-    try:
-        element_present = EC.presence_of_element_located((by_locator(element[0]), element[1]))
-        WebDriverWait(driver, timeout).until(element_present)
-        return True
-    except TimeoutException:
-        return False
-
-
-def find_element(driver, element):
-    return driver.find_element(by_locator(element[0]), element[1])
-
-
-def find_elements(driver, element):
-    return driver.find_elements(by_locator(element[0]), element[1])
-
-
-def web_driver():
-    return webdriver.Chrome()
+    def __init__(self, driver):
+        self.driver = driver
+        self._wait = WebDriverWait(self.driver, 10)
+        self._action = ActionChains(self.driver)
+        
+    def by_locator(self, byclass):
+        if byclass == "NAME":
+            return By.NAME
+        elif byclass == "ID":
+            return By.ID
+        elif byclass == "CLASS_NAME":
+            return By.CLASS_NAME
+        elif byclass == "XPATH":
+            return By.XPATH
+        elif byclass == "TAG_NAME":
+            return By.TAG_NAME    
+    
+    def presence_of_element_located(self, driver, element, timeout):
+        try:
+            element_present = EC.presence_of_element_located((by_locator(element[0]), element[1]))
+            WebDriverWait(driver, timeout).until(element_present)
+            return True
+        except TimeoutException:
+            return False
+        
+    def find_element(self, driver, element):
+        return driver.find_element(by_locator(element[0]), element[1])
+       
+    def find_elements(self, driver, element):
+        return driver.find_elements(by_locator(element[0]), element[1])
+       
+    def web_driver(self):
+        return webdriver.Chrome()
+        
+    def open_url(self, TestData)
+        driver.get(TestData['url'])
